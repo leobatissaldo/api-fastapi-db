20c5fdecb7d70b4b7ba105542afa9fb0fda1ba4c
leobatissaldo <leobatissaldo@gmail.com>
2026-08-23 18:30:31 -0300

    feat: add env

--- auth.py
+++ auth.py
@@ -1,12 +1,20 @@
+import os
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-SECRET_KEY = "secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+load_dotenv()
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+
+if not SECRET_KEY:
+    raise ValueError(
+        "A variável de ambiente SECRET_KEY não foi configurada!"
+    )
 
 def hash_senha(senha: str) -> str:
     return pwd_context.hash(senha)
